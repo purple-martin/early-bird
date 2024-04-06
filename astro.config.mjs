df078e9492515184412ba9629bdde8848a20c060
Keith Robinson <keith.robinson@dtn.com>
2024-04-05 18:54:40 -0700

    Remove UI realated libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify';
 import partytown from '@astrojs/partytown';
 import path from 'path';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import tasks from './src/utils/tasks';
@@ -72,7 +71,6 @@ export default defineConfig({
       Logger: 1,
     }),
     tasks(),
-    react(),
   ],
   image: {
     service: squooshImageService(),
